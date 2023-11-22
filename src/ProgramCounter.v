b983b8b6f3527786a21103b2670209579a185843
StefoKV <151790431+StefoKV@users.noreply.github.com>
2023-11-22 23:33:13 +0100

    Update ProgramCounter.v

--- src/ProgramCounter.v
+++ src/ProgramCounter.v
@@ -1,30 +1,34 @@
 module ProgramCounter(
-	input [31:0] PCNext,
-	input Reset, Clock,PCWrite,
-	output reg [31:0] PC );
+
+	input  wire [7:0] ui_in,    // Dedicated inputs - connected to the input switches
+   	output wire [7:0] uo_out,   // Dedicated outputs - connected to the 7 segment display
+    	input  wire [7:0] uio_in,   // IOs: Bidirectional Input path
+    	output wire [7:0] uio_out,  // IOs: Bidirectional Output path
+    	output wire [7:0] uio_oe,   // IOs: Bidirectional Enable path (active high: 0=input, 1=output)
+    	input  wire       ena,      // will go high when the design is enabled
+    	input  wire       clk,      // clock
+	input  wire       rst_n );     // reset_n - low to reset
 	
 	initial begin
-		PC <= 32'h00000000;
+		uo_out <= 8'h00;
 	end
 
-    always @(posedge Clock)
-    begin
-    	if (Reset)
+	always @(posedge clk)
     	begin
-    		PC <= 32'h00000000;
-    	end
-    	else
-    	begin
-			if (PCWrite == 1) begin
-				PC <= PCNext;
+		if (rst_n)
+    		begin
+    			ou_out <= 8'h00;
+    		end
+    		else
+    		begin
+			if (ena == 1) begin
+				ou_out <= ui_in;
 			end
 			else
 			begin
-				PC += 4;
+				ou_out += 4;
 			end
-    	end
-
-		$display("PC=%h",PC);
-    end
+		end
+	end
 	
 endmodule
